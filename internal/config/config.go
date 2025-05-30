b1371760f0debaa00c39b1d0c2452484b315da15
Dmitry Tumalanov <tumalanov37@gmail.com>
2025-05-30 13:09:30 +0300

    Add port in config

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,6 +17,7 @@ type DB struct {
 	Username string `env:"DB_USERNAME" env-default:"asteriskuser"`
 	Password string `env:"DB_PASSWORD" env-default:"asteriskpassword"`
 	Host     string `env:"DB_HOST" env-default:"localhost"`
+	Port     string `env:"DB_PORT" env-default:"3306"`
 	SourceDB string `env:"DB_SOURCE_DB" env-default:"asterisk_backup"`
 	DestDB   string `env:"DB_DESTINATION_DB" env-default:"asterisk"`
 }
