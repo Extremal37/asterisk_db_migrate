aed8cac48f51c0ecfad78b28ed81fa78677f2ce5
Dmitry Tumalanov <tumalanov37@gmail.com>
2025-05-30 13:18:59 +0300

    Add app

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/Extremal37/asterisk_db_migrate/internal/app"
 	"github.com/Extremal37/asterisk_db_migrate/internal/config"
 	"github.com/Extremal37/asterisk_db_migrate/internal/logger"
 )
@@ -10,19 +11,19 @@ func main() {
 
 	err := cfg.ParseConfig()
 	if err != nil {
-		logger.Fatalf("failed to build app: %s", err)
+		logger.Fatalf("Failed to parse config: %s", err)
 	}
 
 	a := app.New(cfg)
 
 	if err = a.Build(); err != nil {
-		logger.Fatalf("failed to build app: %s", err)
+		logger.Fatalf("Failed to build app: %s", err)
 	}
 
 	if err = a.Run(); err != nil {
-		logger.Fatalf("failed to run app: %s", err)
+		logger.Fatalf("Failed to run app: %s", err)
 	}
 
-	logger.Info("app successfully stopped")
+	logger.Info("App successfully stopped")
 
 }
---
+++ internal/app/app.go
@@ -0,0 +1,47 @@
+package app
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/Extremal37/asterisk_db_migrate/internal/config"
+	"github.com/Extremal37/asterisk_db_migrate/internal/storage"
+)
+
+func New(cfg *config.Config) *App {
+	return &App{
+		cfg: cfg,
+	}
+}
+
+type App struct {
+	cfg     *config.Config
+	storage *storage.Storage
+}
+
+func (a *App) Build() error {
+	conn, err := initDB(a.cfg)
+	if err != nil {
+		return fmt.Errorf("failed to init database: %s", err)
+	}
+
+	a.storage = storage.New(conn)
+
+	return nil
+}
+
+func (a *App) Run() error {
+	err := a.storage.Migrate()
+	if err != nil {
+		return fmt.Errorf("failed to migrate DB: %w", err)
+	}
+	return nil
+}
+
+func initDB(cfg *config.Config) (*sql.DB, error) {
+	conn, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.SourceDB))
+	if err != nil {
+		return conn, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	return conn, nil
+}
